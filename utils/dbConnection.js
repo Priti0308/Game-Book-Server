6f3d674a264a5621f8207be1d581c9a6bbeba033
Priti Kadam <pritikadam0308@gmail.com>
2025-09-09 22:06:31 +0530

    route isuue

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -2,21 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGO_URI_PROD || process.env.MONGO_URI;
-
-    await mongoose.connect(dbURI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    console.log(
-      `✅ MongoDB connected to ${
-        dbURI.includes("localhost") ? "Local (Game-Book)" : "Atlas (Game-Book)"
-      }`
-    );
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
-    process.exit(1); // Stop the server if DB fails
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 };
 
